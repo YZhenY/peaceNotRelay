1f13d52ec3346d1d5bf427ce026c11b692a6eef9
YZhenY <fuz3zy@gmail.com>
2018-07-24 11:11:02 +0800

    added infura node

--- truffle.js
+++ truffle.js
@@ -12,7 +12,33 @@
  *   },
  */
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const private = require('../private.json');
+const mnemonic = private.funds;
+const infuraKey = private.infura;
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+       host: 'localhost',
+       port: 7545,
+       network_id: '*', // Match any network id
+       gas: 3500000,
+     },
+    ropsten: {
+       provider: new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+       network_id: '*',
+       gas: 3500000,
+       gasPrice: 50000000000, // 50 gwei, this is very high
+     },
+     rinkeby: {
+        provider: new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
+        network_id: '*',
+        gas: 3500000,
+        gasPrice: 5000000000, // 50 gwei, this is very high
+      },
+   },
 };
